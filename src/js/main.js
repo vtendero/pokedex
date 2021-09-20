0bca5f20d471698e917867dfbe9f86152dd740e8
vtendero <tendero.valle@gmail.com>
2021-09-20 16:35:22 +0200

    Refactor and add styles

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,8 @@ const pokeId = document.querySelector('.js-pokeId');
 const pokeName = document.querySelector('.js-pokeName');
 const pokeImage = document.querySelector('.js-pokeImage');
 const pokeImageDetail = document.querySelector('.js-pokeImageDetail');
-const pokeNameDetail = document.querySelector('.js-pokeNameDetail');
+const backgroundImage = document.querySelector('.js-backgroundImage');
+const pokeType = document.querySelector('.js-pokeType');
 const pokeTypeHeight = document.querySelector('.js-pokeTypeHeight');
 const pokeTypeWeight = document.querySelector('.js-pokeTypeWeight');
 const backHome = document.querySelector('.js-backHome');
@@ -51,6 +52,9 @@ formElement.addEventListener('submit', handleSearchResults);
 
 function pokemonNotFound() {
   notFound.classList.remove('js-hidden');
+  resultsPokemon.classList.add('js-hidden');
+  buttonDetailElement.classList.add('js-hidden');
+  pokemonsDetail.classList.add('js-hidden');
 }
 
 function getPokemon(pokemon) {
@@ -77,6 +81,7 @@ function renderPokemons(pokemon) {
   pokeName.textContent = `${pokemon.pokeName}`;
   pokeImage.src = `${pokemon.image}`;
   pokeImage.alt = `${pokemon.pokeName} picture`;
+
 }
 
 function handleDetail() {
@@ -99,10 +104,13 @@ function renderPokemonsDetails(pokemon) {
   buttonDetailElement.classList.add('js-hidden');
   pokemonsDetail.classList.remove('js-hidden');
   pokeImageDetail.src = `${pokemon.image}`;
+  backgroundImage.classList.add(pokemon.type + '__back');
   pokeImageDetail.alt = `${pokemon.pokeName} picture`;
-  pokeNameDetail.textContent = `${pokemon.pokeName}`;
-  pokeTypeHeight.textContent = `Height: ${pokemon.height}`;
-  pokeTypeWeight.textContent = `Weight: ${pokemon.weight}`;
+  // pokeNameDetail.textContent = `${pokemon.pokeName}`;
+  pokeType.textContent = `${pokemon.type}`;
+  pokeType.classList.add(pokemon.type);
+  pokeTypeHeight.textContent = `${pokemon.height} dm`;
+  pokeTypeWeight.textContent = `${pokemon.weight} hg`;
   listener();
 }
 
